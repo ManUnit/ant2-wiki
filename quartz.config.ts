13c239fa80afd3ffc21fb1b9c1bc63bffdb133d1
Anan P. <hs1gab@gmail.com>
2026-06-07 23:11:22 +0700

    Set baseUrl to wiki.ant2cloud.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "ant2waf-wiki.vercel.app",
+    baseUrl: "wiki.ant2cloud.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
